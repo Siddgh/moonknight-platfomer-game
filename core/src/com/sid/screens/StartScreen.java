58a9a8a81a613140fd8b1289819665be363eb95d
Siddhesh Dighe <siddheshdighe17@gmail.com>
2023-04-29 17:38:57 -0400

    Detecting & Rendering platform objects

--- core/src/com/sid/screens/StartScreen.java
+++ core/src/com/sid/screens/StartScreen.java
@@ -5,9 +5,15 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -28,6 +34,9 @@ public class StartScreen implements Screen {
     private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
     private float deltaTime;
 
+    private World world;
+    private Box2DDebugRenderer box2DDebugRenderer;
+
     public StartScreen(MainGameClass game) {
         this.game = game;
         gameCamera = new OrthographicCamera();
@@ -38,6 +47,25 @@ public class StartScreen implements Screen {
         levelMap = mapLoader.load("level1/level1.tmx");
         orthogonalTiledMapRenderer = new OrthogonalTiledMapRenderer(levelMap);
         gameCamera.position.set(gameViewport.getWorldWidth() / 2, gameViewport.getWorldHeight() / 2, 0);
+
+        world = new World(new Vector2(0, 0), true);
+        box2DDebugRenderer = new Box2DDebugRenderer();
+
+        BodyDef bodyDef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fixtureDef = new FixtureDef();
+        Body body;
+
+        for (MapObject object : levelMap.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rectangle = ((RectangleMapObject) object).getRectangle();
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rectangle.getX() + rectangle.getWidth() / 2, rectangle.getY() + rectangle.getHeight() / 2);
+            body = world.createBody(bodyDef);
+            shape.setAsBox(rectangle.getWidth() / 2, rectangle.getHeight() / 2);
+            fixtureDef.shape = shape;
+            body.createFixture(fixtureDef);
+        }
+
 
     }
 
@@ -63,9 +91,13 @@ public class StartScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        game.batch.setProjectionMatrix(gameHUD.stage.getCamera().combined);
 
         orthogonalTiledMapRenderer.render();
+
+        box2DDebugRenderer.render(world, gameCamera.combined);
+
+
+        game.batch.setProjectionMatrix(gameHUD.stage.getCamera().combined);
         gameHUD.stage.draw();
     }
 
